2c6a9638cbca546976189f01884fdfb0a3e0d926
LeGrandCharlatan <assassin.Fireteck@gmail.com>
2023-05-14 20:08:04 +0200

    Ajout de boutons d'action

--- src/gui/Editeur.java
+++ src/gui/Editeur.java
@@ -1,12 +1,18 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.util.HashMap;
 
+import javax.swing.Action;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JMenu;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
+import javax.swing.event.CaretListener;
+import javax.swing.text.JTextComponent;
+import javax.swing.text.StyledEditorKit;
 
 import Model.Fiche;
 import Model.FicheSaver;
@@ -30,6 +36,11 @@ public class Editeur extends JPanel {
         JPanel controleurs = new JPanel();
         controleurs.setLayout(new BoxLayout(controleurs, BoxLayout.X_AXIS));
 
+        // Créer la zone d'actions d'édition
+        JPanel actions = new JPanel();
+        actions.setLayout(new BoxLayout(actions, BoxLayout.X_AXIS));
+
+        // Ajout de la sauvegarde et du tire dans les controlleurs
         JTextField title = new JTextField("Titre");
         JButton save = new JButton("Sauvegarder");
         save.addActionListener(e -> {
@@ -39,6 +50,24 @@ public class Editeur extends JPanel {
         controleurs.add(title);
         controleurs.add(save);
         this.add(controleurs, BorderLayout.SOUTH);
+
+        // Ajout des actions d'édition
+        Action boldAction = new StyledEditorKit.BoldAction();
+        boldAction.putValue(Action.NAME, "Bold");
+        JButton bold = new JButton(boldAction);
+        actions.add(bold);
+
+        Action italicAction = new StyledEditorKit.ItalicAction();
+        italicAction.putValue(Action.NAME, "Italic");
+        JButton italic = new JButton(italicAction);
+        actions.add(italic);
+
+        Action underlineAction = new StyledEditorKit.UnderlineAction();
+        underlineAction.putValue(Action.NAME, "Underline");
+        JButton underline = new JButton(underlineAction);
+        actions.add(underline);
+
+        this.add(actions, BorderLayout.NORTH);
 
     }
 }
