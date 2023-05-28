7141811ad5d908f8a7988c5dbfe02efea67ce929
LeGrandCharlatan <assassin.Fireteck@gmail.com>
2023-05-28 15:12:48 +0200

    Ajout controle de taille et de police

--- src/gui/ActionsEdition.java
+++ src/gui/ActionsEdition.java
@@ -2,14 +2,26 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.GraphicsEnvironment;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.Action;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSpinner;
 import javax.swing.JTextPane;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeListener;
+import javax.swing.text.Document;
+import javax.swing.text.MutableAttributeSet;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 import javax.swing.text.StyledEditorKit;
 
 /** Le menu supérieur de la fenêtre d'édition, regroupe tous les boutons
@@ -117,24 +129,80 @@ public class ActionsEdition extends JPanel{
      * On parle ici des commandes de mise en forme du texte. (Gras, italique, etc...)
      */
     private void peuplerEdition(){
+        // Personnalisation de la disposition des boutons
+        this.sousmenu.setLayout(new GridLayout(0, 3));
+
+        // Séparation en trois parties
+        JPanel gauche = new JPanel();
+        JPanel centre = new JPanel();
+        JPanel droite = new JPanel();
 
         // Gras
         Action boldAction = new StyledEditorKit.BoldAction();
         boldAction.putValue(Action.NAME, "Gras");
         JButton bold = new JButton(boldAction);
-        this.sousmenu.add(bold);
+        gauche.add(bold);
 
         // Italique
         Action italicAction = new StyledEditorKit.ItalicAction();
         italicAction.putValue(Action.NAME, "Italique");
         JButton italic = new JButton(italicAction);
-        this.sousmenu.add(italic);
+        gauche.add(italic);
 
         // Souligné
         Action underlineAction = new StyledEditorKit.UnderlineAction();
         underlineAction.putValue(Action.NAME, "Souligné");
         JButton underline = new JButton(underlineAction);
-        this.sousmenu.add(underline);
+        gauche.add(underline);
+
+        // Déclaration du controlleur de police 
+        String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+        JComboBox<String> font = new JComboBox<>(fonts);
+
+        // Ajout d'un spinner de taille de police pour le texte sélectionné
+        SpinnerNumberModel size = new SpinnerNumberModel(12, 1, 100, 1);
+        size.addChangeListener(new ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent e) {
+                textbox.requestFocusInWindow();
+                Document doc = textbox.getDocument();
+                int start = textbox.getSelectionStart();
+                int end = textbox.getSelectionEnd();
+                if (end > start) {
+                    MutableAttributeSet attr = new SimpleAttributeSet();
+                    StyleConstants.setFontSize(attr, size.getNumber().intValue());
+                    ((StyledDocument) doc).setCharacterAttributes(start, end - start, attr, false);
+                }
+            }
+        });
+        JSpinner taille = new JSpinner(size);
+        JPanel taillePanel = new JPanel();
+        JLabel nom = new JLabel("Taille : ");
+        taillePanel.add(nom);
+        taillePanel.add(taille);
+        centre.add(taillePanel);
+        
+        // Ajout d'un spinner pour la police d'écriture
+        font.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                textbox.requestFocusInWindow();
+                Document doc = textbox.getDocument();
+                int start = textbox.getSelectionStart();
+                int end = textbox.getSelectionEnd();
+                if (end > start) {
+                    MutableAttributeSet attr = new SimpleAttributeSet();
+                    StyleConstants.setFontFamily(attr, font.getSelectedItem().toString());
+                    ((StyledDocument) doc).setCharacterAttributes(start, end - start, attr, false);
+                }
+            }
+        });
+        centre.add(font);
+
+
+
+        // Ajout des trois parties
+        this.sousmenu.add(gauche);
+        this.sousmenu.add(centre);
+        this.sousmenu.add(droite);
 
     }
 
