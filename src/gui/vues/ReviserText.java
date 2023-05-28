f234d5f00e68196631a8efb6e8779d4dce288219
LeGrandCharlatan <assassin.Fireteck@gmail.com>
2023-05-28 12:51:45 +0200

    suppression d'imports inutiles

--- src/gui/vues/ReviserText.java
+++ src/gui/vues/ReviserText.java
@@ -5,8 +5,6 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.util.List;
 import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
