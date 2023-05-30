ef19b79889fe4c414d3dd532d3b92333159370ca
Abdelmouhaimen sarhane <68982208+Abdelmouhaimen@users.noreply.github.com>
2023-05-30 11:18:33 +0200

    Ptit fix pour avoir des questions avec HTML

--- src/model/Fiche.java
+++ src/model/Fiche.java
@@ -74,18 +74,20 @@ public class Fiche {
         while (i < taille) {
             if (texte.get(i).contains(balise)) {
                 // Titre de la question
-                String titreq = texte.get(i).substring(balise.length() + 1).strip();
+                String titreq = texte.get(i).substring(texte.get(i).indexOf(balise) + balise.length() + 1).strip();
 
                 List<String> repsV = new LinkedList<>();
                 List<String> repsF = new LinkedList<>();
 
                 // Reponses
-                while (i + 1 < taille && texte.get(i + 1).contains(rep)) {
-                    i++;
-                    if (texte.get(i).charAt(rep.length() + 1) == 'V') {
-                        repsV.add(texte.get(i).substring(rep.length() + 2).strip());
-                    } else if (texte.get(i).charAt(rep.length() + 1) == 'F') {
-                        repsF.add(texte.get(i).substring(rep.length() + 2).strip());
+                while (i + 3 < taille && texte.get(i + 3).contains(rep)) {
+                    i++;i++;i++;
+                    System.out.print(texte.get(i).indexOf(rep) + rep.length() + 1);
+                    
+                    if (texte.get(i).charAt(texte.get(i).indexOf(rep) + rep.length()) == 'V') {
+                        repsV.add(texte.get(i).substring(texte.get(i).indexOf(rep) + rep.length() + 1).strip());
+                    } else if (texte.get(i).charAt(texte.get(i).indexOf(rep) + rep.length()) == 'F') {
+                        repsF.add(texte.get(i).substring(texte.get(i).indexOf(rep) + rep.length() + 1).strip());
                     } else {
                         throw new Error("Mauvaise balise");
                     }
